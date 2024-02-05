25f66372e53972adffe6ce458b46c6c9e389290a
“[Sistemas <adrianseb3@gmail.com>
2024-02-05 09:14:40 +0100

    ejer

--- PROGRAMACION/src/POO_VII/contador_metodo.java
+++ PROGRAMACION/src/POO_VII/contador_metodo.java
@@ -1,5 +1,31 @@
 package POO_VII;
 
 public class contador_metodo {
+	public static boolean esNumero(String cadena) {
+		try {
+			Double.parseDouble(cadena);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 
+	public static boolean estaEnRango(double numero, double rangoInicio, double rangoFin) {
+		return numero >= rangoInicio && numero <= rangoFin;
+	}
+
+	public static void main(String[] args) {
+		String cadena1 = "123";
+		String cadena2 = "abc";
+		double numero1 = 15.5;
+		double numero2 = 8.0;
+		double rangoInicio = 10.0;
+		double rangoFin = 20.0;
+		System.out.println("¿" + cadena1 + " es un número? " + esNumero(cadena1));
+		System.out.println("¿" + cadena2 + " es un número? " + esNumero(cadena2));
+		System.out.println("¿" + numero1 + " está en el rango (" + rangoInicio + ", " + rangoFin + ")? "
+				+ estaEnRango(numero1, rangoInicio, rangoFin));
+		System.out.println("¿" + numero2 + " está en el rango (" + rangoInicio + ", " + rangoFin + ")? "
+				+ estaEnRango(numero2, rangoInicio, rangoFin));
+	}
 }
--- PROGRAMACION/src/POO_VII/cuenta_instancias.java
+++ PROGRAMACION/src/POO_VII/cuenta_instancias.java
@@ -1,21 +1,19 @@
 package POO_VII;
 
 public class cuenta_instancias {
-	private static int contadorDeInstancias = 0; 
-	public cuenta_instancias() { 
-	contadorDeInstancias++; 
-	} 
+	private static int contadorDeInstancias = 0;
 
-	public static int obtenerNumeroDeInstancias() { 
-	return contadorDeInstancias; 
-	} 
+	public cuenta_instancias() {
+		contadorDeInstancias++;
+	}
 
-	public static void main(String[] args) { 
-	cuenta_instancias prubea1 = new cuenta_instancias(); 
+	public static int obtenerNumeroDeInstancias() {
+		return contadorDeInstancias;
+	}
 
-	cuenta_instancias prueba2 = new cuenta_instancias(); 
-
-	System.out.println("Número de instancias: " + cuenta_instancias.obtenerNumeroDeInstancias()); 
-
-	} 
+	public static void main(String[] args) {
+		cuenta_instancias prubea1 = new cuenta_instancias();
+		cuenta_instancias prueba2 = new cuenta_instancias();
+		System.out.println("Número de instancias: " + cuenta_instancias.obtenerNumeroDeInstancias());
+	}
 }
